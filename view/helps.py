99e89dbc9b958bf5f81341de7a8902dffbf98400
DESKTOP-HGC2TT6\kun <zoukun2001@outlook.com>
2023-03-08 13:03:49 +0800

    版本1.1: 修改帮助

--- view/helps.py
+++ view/helps.py
@@ -27,7 +27,22 @@ class Help(QDialog):
         self.ok.clicked.connect(self.close)
 
     def __set_text(self):
-        self.text.setText("<h1>Bookmarks</h1>pdf 目录添加工具")
+        self.text.setText(""
+                          "<body "
+                          " style = "
+                          " font-style:normal;"
+                          ">"
+                          "<h1>Bookmarks</h1>"
+                          "<p>  pdf 给 pdf 文件添加目录。</p>"
+                          "<h2>导出目录</h2>"
+                          "<p>  将当前的pdf文件目录导出指定到txt文件。</p>"
+                          "<h2>导入目录</h2>"
+                          "<p>  将txt文件的目录导入到下方的目录树编辑器中。</p>"
+                          "<p>  txt文件中以tab缩进来区分文件目录层级。</p>"
+                          "<h2>保存文件</h2>"
+                          "<p>  生成当前文件副本，并将目录树编辑器的目录保存到pdf副本中。</p>"
+                          "</body>"
+                          )
 
 
 
